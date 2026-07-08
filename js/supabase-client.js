ab66058fd273ebadede5f1a70b431587d12d02ff
federicoolego <98785348+federicoolego@users.noreply.github.com>
2026-07-08 13:58:00 -0300

    Create supabase-client.js

---
+++ js/supabase-client.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = "https://puofvxsvoaaynbspddsj.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_n9I9IHiVTBD_SRr1znNvyQ_Xvo6-Sls"; // Es pública, va acá tranquilo
+window.sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
